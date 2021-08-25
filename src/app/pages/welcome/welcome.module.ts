f0136338ac2f9c0701ea9d8a85f9edf81fade3d7
Burak Mert Muş <burakmertmus@gmail.com>
2021-08-25 16:26:51 +0300

    Added İcons

--- src/app/pages/welcome/welcome.module.ts
+++ src/app/pages/welcome/welcome.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 
 import { WelcomeRoutingModule } from './welcome-routing.module';
 
 import { WelcomeComponent } from './welcome.component';
-
-
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzGridModule } from 'ng-zorro-antd/grid';
 @NgModule({
-  imports: [WelcomeRoutingModule],
+  imports: [WelcomeRoutingModule,NzButtonModule,NzCardModule,NzGridModule],
   declarations: [WelcomeComponent],
   exports: [WelcomeComponent]
 })
